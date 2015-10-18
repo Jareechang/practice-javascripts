4a5edbde2caecee46bf29a02f4fbad8d693da9a7
Jerry Chang <therealjerrycan@gmail.com>
2015-10-17 22:45:59 -0700

    updated the restModule with a returnResponse public method

--- learnyounode_8.js
+++ learnyounode_8.js
@@ -3,6 +3,18 @@
 
 restModule = require('./rest_module.js');
 
-requestEndPoint = process.argv[2];
+url = process.argv[2];
 
-console.log( restModule.sentGetRequest(requestEndPoint) );
+/* 
+** 
+    Callback function to read the variables upon 
+    finishing the Get request to 'x' url
+**
+*/
+
+returnData = function(data){
+    // do something to data set...
+    
+}
+
+restModule.returnResponse( url, returnData);
--- rest_module.js
+++ rest_module.js
@@ -3,58 +3,76 @@
 // Require the http core module
 var http = require('http');
 
-// var req = http.get(process.argv[2], callback);
-// HTTP module  
 
-    // Takes in a request from the command line, 
-        // ––– Defined by EX. var request = process.argv[2];
-        // ––– httpApi(request);
+/* HTTP module  
+*    
+*   Uses Singleton to which is a self executing function 
+*       
+*         
+*   var request = process.argv[2];
+*        
+*   RestModule.get(request);      // the response is available via a reader method `returnResponse`
+*   
+*   
+*/
 
-    var restModule = (function(){
+
+var restModule = (function(){
+
+    var attribute = {};
+    
+    function validated(request){
         
-        function validated(request){
+        if (typeof request == 'undefined') { return; }
 
-            if (typeof request == 'undefined') {
-                return;
-            }
+        var pattern =  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+        var stringValid = request.match(pattern); 
 
-            var pattern =  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
-            var stringValid = request.match(pattern); 
+        return stringValid ? true : false;  // Check if string is a valid request 
+    }
 
-            return stringValid ? true : false;  // Check if string is a valid request 
-        }
+    function _callback(res){    
 
-        function _callback(res){
-
-            res.setEncoding('utf8');
-            
-            res.on('data', function(data){
-                console.log(data);
-            });                
-
-            res.on('error', function(e) {
-                console.log('problem with request: ' + e.message);
-            });
-        }
+        res.setEncoding('utf8');
         
-        function sentGetRequest(request) {
+        res.on('data', function(data){   
+
+            attribute.get(data);                                  
             
-            if( validated(request) ){
+            // console.log(data);  // for logging the data 
+        });                
 
-                http.get(request, _callback);
+        res.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+    }
 
-            }else {
+    
+    function sentGetRequest(request) {
+        
+        if( validated(request) ){ http.get(request, _callback); }
 
-                console.log("Request invalid, please remember to pass in third argument with proper protocol" +
-                            "(http://) along with proper domain (google.com)");
+        else {
+            console.log("Request invalid, please remember to pass in third argument with proper protocol" +
+                        "(http://) along with proper domain (google.com)");
+        }
+    }        
 
-            }
-        }        
-        // Return the Interface to interact with module
-        return {
-            sentGetRequest : sentGetRequest
-        };
-    })(http);
+    function returnResponse(request, callback) {
+        
+        attribute.get = callback;
+
+        sentGetRequest(request);
+
+    }
+
+    // Return the API Interface to interact with module
+    return {
+        get: sentGetRequest,
+        returnResponse: returnResponse            
+    };
+
+})(http);
 
 
 //Exporting module
